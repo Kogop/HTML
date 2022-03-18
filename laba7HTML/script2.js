dac1a410a35f07b8882fc86ef5d73353945839ab
Nesterov Vlad <nesterovvlad100@gmail.com>
2022-03-18 13:59:37 +0300

    popa

---
+++ laba7HTML/script2.js
@@ -0,0 +1,12 @@
+let a1 = prompt("Vvedite 1 4islo", );
+let a2 = prompt("Vvedite 2 4islo", );
+
+if (a1*a2 ==0) {
+    alert("0");
+    
+}else if(a1*a2 >0){
+    alert("1");
+
+}else{
+    alert("-1");
+}

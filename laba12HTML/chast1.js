d416f1e95af1ac2c6e79c89f3116ca6992602928
Vladislav Nesterov <nesterovvlad100@gmail.com>
2022-05-05 17:33:43 +0300

    chast 1 gotova krivo vivodit no gotovo

--- laba12HTML/chast1.js
+++ laba12HTML/chast1.js
@@ -1,9 +1,16 @@
 let N = prompt("введите размер матрицы");
+var arr = [];
 
+//arr = Array(N).fill(Array(N));
 
-var arr = Array(N).fill(Array(N));
+for (let i = 0; i < N; i++) { 
+    if (!arr[i]) arr[i] = [];
+    for (let j = 0; j < N; j++) {
+        arr[i][j] = (i+1)*(j+1);
+        alert(arr[i][j]);
+    }
+}
 
+//let r = arr[3][3];
 
-arr[0][0] = 1;
-arr[1][0] = 2;
-alert(arr[1][0]);
+//alert(r);

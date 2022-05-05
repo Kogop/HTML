2d11e20c649571b7b07c3114526335c590b20c58
Vladislav Nesterov <nesterovvlad100@gmail.com>
2022-05-05 18:37:13 +0300

    1st done. not exactly but done.

--- laba12HTML/chast1.js
+++ laba12HTML/chast1.js
@@ -2,15 +2,19 @@ let N = prompt("введите размер матрицы");
 var arr = [];
 
 //arr = Array(N).fill(Array(N));
-
-for (let i = 0; i < N; i++) { 
-    if (!arr[i]) arr[i] = [];
-    for (let j = 0; j < N; j++) {
-        arr[i][j] = (i+1)*(j+1);
-        alert(arr[i][j]);
+function MakeTablUmnoj(n) {
+    for (let i = 0; i < n; i++) { 
+        if (!arr[i]) arr[i] = [];
+        for (let j = 0; j < n; j++) {
+            arr[i][j] = (i+1)*(j+1);
+            //alert(arr[i][j]);
+        }
+        //alert(arr[i]);
     }
+    //var r = arr;
+    return arr;
 }
 
 //let r = arr[3][3];
 
-//alert(r);
+alert(MakeTablUmnoj(N));

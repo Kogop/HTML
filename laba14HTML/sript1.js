412031704e6ef39b4596fe8cd0fdd72cb6c7931b
Nesterov Vlad <nesterovvlad100@gmail.com>
2022-05-06 14:55:53 +0300

    14 sdana

--- laba14HTML/sript1.js
+++ laba14HTML/sript1.js
@@ -8,11 +8,12 @@ let sud = [[5, 3, 4, 6, 7, 8, 9, 1, 2],
 [2, 8, 7, 4, 1, 9, 6, 3, 5],
 [3, 4, 5, 2, 8, 6, 1, 7, 9]
 ];
-let arr_1 = [1,1,1,1,1,1,1,1,1];
+//let arr_1 = [1,1,1,1,1,1,1,1,1];
+
+
 
 function IsSudokuTrue(sudoku) {
     
-
     for (let i = 0; i < 9; i++) { 
         let arr = [0,0,0,0,0,0,0,0,0];
         for (let j = 0; j < 9; j++) {
@@ -25,7 +26,7 @@ function IsSudokuTrue(sudoku) {
            }
             //alert(arr[i][j]);  
         }
-        if(arr_1.every(elem =>elem == 1) == arr.every(elem =>elem == 1)){
+        if(/*arr_1.every(elem =>elem == 1) == */arr.every(elem =>elem == 1)){
                 // hren napisal every i tak vozvrashaet true tak 4to proveriat mojno
                 // i nado tolko sam arr
             
@@ -33,7 +34,7 @@ function IsSudokuTrue(sudoku) {
             return false;
         }
         //alert(arr[i]);
-    }
+    };
     
     for (let j = 0; j < 9; j++) { 
         let arr = [0,0,0,0,0,0,0,0,0];
@@ -47,13 +48,47 @@ function IsSudokuTrue(sudoku) {
            }
             //alert(arr[i][j]);  
         }
-        if(arr_1.every(elem =>elem == 1) == arr.every(elem =>elem == 1)){
+        if(/*arr_1.every(elem =>elem == 1) == */arr.every(elem =>elem == 1)){
 
             
         }else{
             return false;
         }
         //alert(arr[i]);
+    };
+
+    let a = 0, b = 0, arrB;
+    for (let g = 0; g < 9; g++) {
+        arrB = [0,0,0,0,0,0,0,0,0];
+        for (let i = a; i < a+3; i++) { 
+            
+            for (let j = b; j < b+3; j++) {
+
+                for (let k = 0; k < 9; k++) {
+                   // console.log(g,i,j,a,b,k)
+                    if (sudoku[i][j] == k+1) {
+                        arrB[k] += 1;
+                        //console.log(arr[k]);
+                    }  
+                }  
+            }
+            console.log(arrB);
+           
+        }  
+        a+=3;
+        //console.log(arr);  
+        if(/*arr_1.every(elem =>elem == 1) == */arrB.every(elem =>{ console.log(elem); return elem == 1})){
+            console.log(b);
+        }else{
+            return false;
+        }
+        //console.log(b);
+        if (a==9) {
+            b = b+3;
+            a = 0;
+            //console.log(b);
+        }
+        arrB = [0,0,0,0,0,0,0,0,0];
     }
     return true;
 };

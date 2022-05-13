57514e50ffe7f7670106c9de4e208f85ebdc3602
Nesterov Vlad <nesterovvlad100@gmail.com>
2022-05-13 14:09:34 +0300

    spigeno i rabotaet peredelat mojno po crasivee

--- laba15HTML/script.js
+++ laba15HTML/script.js
@@ -4,9 +4,15 @@ let openPopupButtons = document.querySelectorAll('.open-popup'); // Кнопки
 let closePopupButton = document.querySelector('.close-popup'); // Кнопка для скрытия окна
 openPopupButtons.forEach((button) => { // Перебираем все кнопки
     button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
+        console.log("test");
+        let mail = document.getElementById("mailid");
+        //console.log(mail.value);
+        let mail2 = document.getElementById("mail2");
+        mail2.value = mail.value;
         e.preventDefault(); // Предотвращаем дефолтное поведение браузера
         popupBg.classList.add('active'); // Добавляем класс 'active' для фона
         popup.classList.add('active'); // И для самого окна
+
     })
 });
 closePopupButton.addEventListener('click',() => { // Вешаем обработчик на крестик
@@ -14,8 +20,12 @@ closePopupButton.addEventListener('click',() => { // Вешаем обработ
     popup.classList.remove('active'); // И с окна
 });
 document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
-    if(e.target === popupBg) { // Если цель клика - фот, то:
+    if(e.target === popupBg) { // Если цель клика - фон, то:
         popupBg.classList.remove('active'); // Убираем активный класс с фона
         popup.classList.remove('active'); // И с окна
     }
 });
+function mes(event){
+    event.preventDefault();
+    console.log("Регистрация прошла успешно");
+}

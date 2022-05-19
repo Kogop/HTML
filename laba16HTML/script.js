e47453e6b3b70c29384aca3c32203238ac48684a
Vladislav Nesterov <nesterovvlad100@gmail.com>
2022-05-19 21:40:45 +0300

    done

--- laba16HTML/script.js
+++ laba16HTML/script.js
@@ -1,8 +1,20 @@
-let mas = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"];
+let mas = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST","EAST", "NORTH", "WEST","WEST","EAST"];
 
 function Sokrawenie_Puti(mass) {
-
-    
+for (let j = 0; j < mass.length; j++) {
+  
+    for (let i = 0; i < mass.length; i++) {
+        if ((mass[j] == "NORTH" && mass[i] == "SOUTH")||(mass[i] == "NORTH" && mass[j] == "SOUTH")) {
+            mass.splice(i,1);
+            mass.splice(j,1);
+        };
+        if ((mass[j] == "EAST" && mass[i] == "WEST")||(mass[i] == "EAST" && mass[j] == "WEST")) {
+            mass.splice(i,1);
+            mass.splice(j,1);
+        };
+        
+    }
+}
 
 
     return mass;

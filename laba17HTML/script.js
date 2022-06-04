9e2773e7531d4de38a0aec4175d1c53265634587
Vladislav Nesterov <nesterovvlad100@gmail.com>
2022-06-04 18:38:16 +0300

    button exist. now make her on new things

--- laba17HTML/script.js
+++ laba17HTML/script.js
@@ -1,52 +1,73 @@
-const todo = {
-    action(e) {
-      const target = e.target;
-      if (target.classList.contains('todo__action')) {
-        const action = target.dataset.todoAction;
-        const elemItem = target.closest('.todo__item');
-        if (action === 'deleted' && elemItem.dataset.todoState === 'deleted') {
-          elemItem.remove();
-        } else {
-          elemItem.dataset.todoState = action;
-        }
-        this.save();
-      } else if (target.classList.contains('todo__add')) {
-        this.add();
-        this.save();
-      }
-    },
-    add() {
-      const elemText = document.querySelector('.todo__text');
-      if (elemText.disabled || !elemText.value.length) {
-        return;
-      }
-      document.querySelector('.todo__items').insertAdjacentHTML('beforeend', this.create(elemText.value));
-      elemText.value = '';
-    },
-    create(text) {
-      return `<li class="todo__item" data-todo-state="active">
-        <span class="todo__task">${text}</span>
-        <span class="todo__action todo__action_restore" data-todo-action="active"></span>
-        <span class="todo__action todo__action_complete" data-todo-action="completed"></span>
-        <span class="todo__action todo__action_delete" data-todo-action="deleted"></span>
-        <span class="todo__action todo__action_copy" data-todo-action="copy"></span></li>`;
-    },
-    init() {
-      const fromStorage = localStorage.getItem('todo');
-      if (fromStorage) {
-        document.querySelector('.todo__items').innerHTML = fromStorage;
-      }
-      document.querySelector('.todo__options').addEventListener('change', this.update);
-      document.addEventListener('click', this.action.bind(this));
-    },
-    update() {
-      const option = document.querySelector('.todo__options').value;
-      document.querySelector('.todo__items').dataset.todoOption = option;
-      document.querySelector('.todo__text').disabled = option !== 'active';
-    },
-    save() {
-      localStorage.setItem('todo', document.querySelector('.todo__items').innerHTML);
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+// Create a "copy" button and append it to each list item
+var myNodelist1 = document.getElementsByTagName("LI");
+
+for (let i = 0; i < myNodelist1.length; i++) {
+  var span1 = document.createElement("SPAN");
+  var txt1 = document.createTextNode("\u00D7");
+  span1.className = "copy";
+  span1.appendChild(txt1);
+  myNodelist1[i].appendChild(span1);
+}
+// Click on a copy button to copy the current list item
+var copy = document.getElementsByClassName("copy");
+
+for (let i = 0; i < close.length; i++) {
+  copy[i].onclick = function() {
+    var div = this.parentElement;
+    document.getElementById("myUL").appendChild(li)
+  }
+}
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
     }
   }
-  
-  todo.init();
+}

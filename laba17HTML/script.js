d7ee9cb4fc6b110255d1aea8a2fc6260e97fcb2c
Vladislav Nesterov <nesterovvlad100@gmail.com>
2022-06-04 19:05:48 +0300

    copyes but shows undefined

--- laba17HTML/script.js
+++ laba17HTML/script.js
@@ -8,25 +8,18 @@ for (i = 0; i < myNodelist.length; i++) {
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
+/*
 // Create a "copy" button and append it to each list item
 var myNodelist1 = document.getElementsByTagName("LI");
-
-for (let i = 0; i < myNodelist1.length; i++) {
+var i;
+for (i = 0; i < myNodelist1.length; i++) {
   var span1 = document.createElement("SPAN");
   var txt1 = document.createTextNode("\u00D7");
   span1.className = "copy";
   span1.appendChild(txt1);
   myNodelist1[i].appendChild(span1);
-}
-// Click on a copy button to copy the current list item
-var copy = document.getElementsByClassName("copy");
+}*/
 
-for (let i = 0; i < close.length; i++) {
-  copy[i].onclick = function() {
-    var div = this.parentElement;
-    document.getElementById("myUL").appendChild(li)
-  }
-}
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
 var i;
@@ -63,7 +56,13 @@ function newElement() {
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
-
+/* knopki copy na cajdoy plawke
+  var span1 = document.createElement("SPAN");
+  var txt1 = document.createTextNode("\u00D7");
+  span1.className = "copy";
+  span1.appendChild(txt1);
+  li.appendChild(span1);
+*/
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
       var div = this.parentElement;
@@ -71,3 +70,35 @@ function newElement() {
     }
   }
 }
+// Create a copy of checked list item when clicking on the "Copy" button
+function newElement1() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementsByClassName("checked").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  /*   knopki copy na cajdoy plawke
+    var span1 = document.createElement("SPAN");
+    var txt1 = document.createTextNode("\u00D7");
+    span1.className = "copy";
+    span1.appendChild(txt1);
+    li.appendChild(span1);
+  */
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
